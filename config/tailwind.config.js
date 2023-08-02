4a937325875a6beee3985d83d94c6f82a00c3247
Robert <robarangs57@gmail.com>
2023-08-02 16:52:21 +0300

    hero-section: start

---
+++ config/tailwind.config.js
@@ -0,0 +1,21 @@
+tailwind.config = {
+    theme: {
+        extend: {
+            colors: {
+                primary: {
+                    dark: '#1f1f2a',
+                    blue: '#00aac2',
+                    light: '#C3C9CC'
+                },
+                secondary: {
+                    gray: '#8C949C',
+                    blue: '#048B9C',
+                    dark: '#43414D'
+                }
+            },
+            container: {
+                center: true
+            }
+        }
+    }
+}
